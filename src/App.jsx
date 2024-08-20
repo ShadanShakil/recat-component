b8c0893af84e90641ba192ec2855aeb2a8e4f986
Khawaja Shadan Shakil <shadanshakil9@gmail.com>
2024-08-20 21:42:55 +0500

    react component assignment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Card
+          title="React Component"
+          description="React apps are made out of components. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page."
+          image="https://kinsta.com/wp-content/uploads/2023/04/react-must-be-in-scope-when-using-jsx.jpg"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
